f838dde71211ff419f07d364283c30ca58bd7ad1
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-26 11:51:09 -0800

    IIFE to create navbar

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./style.css";
+let content = document.getElementById('content');
+
+let createNav = (function(){
+    let nav = document.createElement('nav');
+    let logo = document.createElement('div');
+    logo.textContent = 'pizzaPizza';
+    logo.setAttribute('id', 'logo');
+    let ul = document.createElement('ul');
+    let navLinks = ['HOME', 'MENU', 'CONTACT'];
+    let li;
+    for(let i = 0; i < 3; i++){
+        li = document.createElement('li');
+        li.textContent = navLinks[i];
+        ul.appendChild(li);
+    };
+    nav.appendChild(logo);
+    nav.appendChild(ul);
+    content.appendChild(nav);
+
+})()
