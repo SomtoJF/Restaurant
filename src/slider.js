2a567e9f5c22078d9209f0525ff9fd0e5bbe1c07
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-27 09:03:02 -0800

    Create and style slider

--- src/slider.js
+++ src/slider.js
@@ -1,8 +1,10 @@
-let content = document.getElementById('content');
-
 let createSlider = function (){
     let sliderContainer = document.createElement('div');
     sliderContainer.setAttribute('id', 'slider-container');
+    for(let i = 0; i < 5; i++){
+        let sliderItem = document.createElement('div');
+        sliderContainer.appendChild(sliderItem);
+    }
     return sliderContainer;
 };
 export default createSlider;
