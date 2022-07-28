6cb4a362e17de94ff5aa9f00d30fd86dbc5c3b24
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-28 00:09:17 -0800

    Import nav module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import "./style.css";
 import "./slider.css";
+import {default as createNav} from './nav';
 import {default as createSlider} from "./slider";
+import {default as createHomeContent} from "./homeContent"
 
 let content = document.getElementById('content');
 
-let createNav = (function(){
-    let nav = document.createElement('nav');
-    let logo = document.createElement('div');
-    logo.textContent = 'pizzaPizza';
-    logo.setAttribute('id', 'logo');
-    let ul = document.createElement('ul');
-    let navLinks = ['HOME', 'MENU', 'CONTACT US'];
-    let li;
-    for(let i = 0; i < 3; i++){
-        li = document.createElement('li');
-        li.textContent = navLinks[i];
-        ul.appendChild(li);
-    };
-    nav.appendChild(logo);
-    nav.appendChild(ul);
-    content.appendChild(nav);
-
-})()
-
+createNav();
 content.appendChild(createSlider());
+content.appendChild(createHomeContent());
