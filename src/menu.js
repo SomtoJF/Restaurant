6429056e0e6ba14ed99d2aed562f4258e7680c32
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-29 08:22:01 -0800

    Bugfix, Feature: Module imports drinks from wrong location and create drink items

--- src/menu.js
+++ src/menu.js
@@ -3,10 +3,16 @@ import cheese from './toppings/cheese.png';
 import mushroom from './toppings/mushroom.png';
 import tomato from './toppings/tomato.png';
 import veggies from './toppings/veggies.png';
+import coke from './drinks/coke.png';
+import fanta from './drinks/fanta.png';
+import juice from './drinks/juice.png';
+import smoothie from './drinks/smoothie.png';
+import yoghurt from './drinks/yoghurt.png';
 
 let createMenu = function (){
     let toppings = [pepperoni, cheese, mushroom, tomato, veggies];
-    let sizes = ['Small', 'Medium', 'Large']
+    let sizes = ['Small', 'Medium', 'Large'];
+    let drinks = [coke, fanta, juice, smoothie, yoghurt];
     let container = document.createElement('div');
     container.setAttribute('id', 'menu-container');
     for(let i = 0; i < 3; i++){
@@ -29,7 +35,16 @@ let createMenu = function (){
                 toppingsDiv.textContent = `${sizes[j]}`;
                 smallerContainer.appendChild(toppingsDiv);
             };
-        }
+        };
+        if(i == 2){
+            title.textContent = 'Drinks';
+            smallerContainer.appendChild(title);
+            for(let j = 0; j < drinks.length; j++){
+                let toppingsDiv = document.createElement('div');
+                toppingsDiv.style.backgroundImage = `url(${drinks[j]})`;
+                smallerContainer.appendChild(toppingsDiv);
+            };
+        };
         container.appendChild(smallerContainer);
     };
     return container;
